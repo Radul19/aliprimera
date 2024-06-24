ba17d441824696e02a84d3145a56d57542f2f70d
Radul19 <radulito19@gmail.com>
2024-06-24 18:39:52 -0400

    optimized a lot

--- src/routes/guest.routes.ts
+++ src/routes/guest.routes.ts
@@ -5,8 +5,8 @@ import *as guest from '../controllers/guest'
 
 router.get('/', guest.test)
 router.get('/cartelera_informativa', guest.test)
-router.get('/negocios/:categ?', guest.test)
-router.get('/negocio/:categ?', guest.test)
+router.get('/emprendimientos/:categ?', guest.test)
+router.get('/emprendimiento/:categ?', guest.test)
 router.get('/contacto', guest.test)
 // router.get('/contacto', guest.test)
 
