8b3e1672d44a7302f2913a2f707f5e076a46cad5
Radul19 <radulito19@gmail.com>
2024-06-24 06:09:39 -0400

    Better design

--- src/controllers/guest.ts
+++ src/controllers/guest.ts
@@ -1,9 +1,13 @@
 import { RequestHandler } from "express"
+import path from "path";
 const debug = false
+
+const indexPath = path.resolve(__dirname, "../app", "index.html");
+// console.log(indexPath);
 
 export const test: RequestHandler = async (req, res) => {
     if (debug) console.log('#test')
-    res.send('')
+    res.sendFile(indexPath)
 }
 
 // export const namehere = async (req,res)=>{
--- src/routes/guest.routes.ts
+++ src/routes/guest.routes.ts
@@ -3,7 +3,11 @@ const router = Router()
 
 import *as guest from '../controllers/guest'
 
-router.get('/*', guest.test)
+router.get('/', guest.test)
+router.get('/cartelera_informativa', guest.test)
+router.get('/negocios/:categ?', guest.test)
+router.get('/negocio/:categ?', guest.test)
+router.get('/contacto', guest.test)
 // router.get('/contacto', guest.test)
 
 export default router
