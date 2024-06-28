d00fa93a8ff8ec5bb003d8d0ed669922d5d7bb96
Radul19 <radulito19@gmail.com>
2024-06-28 07:52:17 -0400

    map + hover + footer

--- src/controllers/guest.ts
+++ src/controllers/guest.ts
@@ -1,21 +1,29 @@
-import { RequestHandler } from "express"
+import { RequestHandler } from "express";
 import path from "path";
-const debug = false
+const debug = false;
 
 const indexPath = path.resolve(__dirname, "../app", "index.html");
 // console.log(indexPath);
 
 export const test: RequestHandler = async (req, res) => {
-    if (debug) console.log('#test')
-    res.sendFile(indexPath)
-}
+//   if (debug) console.log("#test");
+  res.sendFile(indexPath);
+};
+export const createUser: RequestHandler = async (req, res) => {
+  if (debug) console.log("#createUser");
+  try {
+    res.send("ok");
+  } catch (error: any) {
+    res.status(400).json({ msg: error.message });
+  }
+};
+// export const zzzzzzz: RequestHandler = async (req, res) => {
+//   if (debug) console.log("#test");
+//   try {
+//     res.send("ok");
+//   } catch (error: any) {
+//     res.status(400).json({ msg: error.message });
+//   }
+// };
 
-// export const namehere = async (req,res)=>{
-//     if(debug) console.log('#namehere')
-//     try {
-//         res.send('ok')
-//     } catch (error:any) {
-//         res.status(400).json({ msg: error.message })
-//     }
-//
-// }
+/**tesispass123 */
