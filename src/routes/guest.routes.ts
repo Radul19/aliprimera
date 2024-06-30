fc37e638cee5f8a2c4465b63897bcafb7efbd695
Radul19 <radulito19@gmail.com>
2024-06-30 08:31:40 -0400

    admin

--- src/routes/guest.routes.ts
+++ src/routes/guest.routes.ts
@@ -8,6 +8,7 @@ router.get('/cartelera_informativa', guest.test)
 router.get('/emprendimientos/:categ?', guest.test)
 router.get('/emprendimiento/:categ?', guest.test)
 router.get('/contacto', guest.test)
+router.get('/admin', guest.test)
 // router.get('/contacto', guest.test)
 
 export default router
