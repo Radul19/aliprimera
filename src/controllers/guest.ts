880e4bf7cc48e48ef5006fe433fe6e2af332a2a6
Radul19 <radulito19@gmail.com>
2024-07-02 09:31:56 -0400

    markets + login

--- src/controllers/guest.ts
+++ src/controllers/guest.ts
@@ -1,9 +1,17 @@
 import { RequestHandler } from "express";
 import path from "path";
 import ImageSch from "../models/ImageSch";
-import { deleteImage, uploadImage } from "../helpers/uploadImages";
+import {
+  deleteImage,
+  deleteImages,
+  uploadImage,
+} from "../helpers/uploadImages";
 import NewSch from "../models/NewSch";
 import ChartSch from "../models/ChartSch";
+import MarketSch from "../models/MarketSch";
+//@ts-ignore
+import bcrypt from "bcrypt";
+import UserSch from "../models/UserSch";
 const debug = true;
 
 const indexPath = path.resolve(__dirname, "../app", "index.html");
@@ -16,11 +24,14 @@ export const test: RequestHandler = async (req, res) => {
 export const loadAll: RequestHandler = async (req, res) => {
   if (debug) console.log("#loadAll");
   try {
-    
-    await ChartSch.findOneAndUpdate({ _id: "6682def3e5b948a57d5e16cc" }, { $inc: { amount: 1 } });
+    await ChartSch.findOneAndUpdate(
+      { _id: "6682def3e5b948a57d5e16cc" },
+      { $inc: { amount: 1 } }
+    );
     const gallery = await ImageSch.find();
     const news = await NewSch.find();
-    res.send({ gallery, news });
+    const markets = await MarketSch.find();
+    res.send({ gallery, news, markets });
   } catch (error: any) {
     res.status(400).json({ msg: error.message });
   }
@@ -124,6 +135,157 @@ export const delNews: RequestHandler = async (req, res) => {
     res.status(400).json({ msg: error.message });
   }
 };
+
+/** MARKET */
+export const createMarket: RequestHandler = async (req, res) => {
+  if (debug) console.log("#createMarket");
+  try {
+    const {
+      name,
+      description,
+      logo: logoData,
+      front: frontData,
+      map,
+      ws,
+      ig,
+      category,
+    } = req.body;
+    const logo = await uploadImage({
+      secure_url: logoData.secure_url,
+      public_id: logoData.public_id,
+    });
+    const front = await uploadImage({
+      secure_url: frontData.secure_url,
+      public_id: frontData.public_id,
+    });
+
+    await MarketSch.create({
+      name,
+      description,
+      logo,
+      front,
+      map,
+      ws,
+      ig,
+      category,
+    });
+    const result = await MarketSch.find();
+    res.send(result);
+  } catch (error: any) {
+    res.status(400).json({ msg: error.message });
+  }
+};
+export const updateMarket: RequestHandler = async (req, res) => {
+  if (debug) console.log("#updateMarket");
+  try {
+    const {
+      _id,
+      name,
+      description,
+      logo: logoData,
+      front: frontData,
+      map,
+      ws,
+      ig,
+      category,
+    } = req.body;
+    const logo = await uploadImage({
+      secure_url: logoData.secure_url,
+      public_id: logoData.public_id,
+    });
+    const front = await uploadImage({
+      secure_url: frontData.secure_url,
+      public_id: frontData.public_id,
+    });
+
+    await MarketSch.findOneAndUpdate(
+      { _id },
+      { name, description, logo, front, map, ws, ig, category }
+    );
+    const result = await MarketSch.find();
+    res.send(result);
+  } catch (error: any) {
+    res.status(400).json({ msg: error.message });
+  }
+};
+export const delMarket: RequestHandler = async (req, res) => {
+  if (debug) console.log("#delMarket");
+  try {
+    const { front, logo, gallery, _id } = req.body;
+    // await deleteImage(public_id);
+    await deleteImages([...gallery, front, logo]);
+
+    await MarketSch.findOneAndDelete({ _id });
+    const result2 = await MarketSch.find();
+    res.send(result2);
+  } catch (error: any) {
+    res.status(400).json({ msg: error.message });
+  }
+};
+export const uploadImageMarket: RequestHandler = async (req, res) => {
+  if (debug) console.log("#uploadImageMarket");
+  try {
+    const { secure_url: su, _id } = req.body;
+    const image = await uploadImage({ secure_url: su, public_id: "" });
+    await MarketSch.findOneAndUpdate(
+      { _id },
+      {
+        $push: { gallery: image },
+      }
+    );
+    const result = await MarketSch.find();
+    res.send(result);
+  } catch (error: any) {
+    res.status(400).json({ msg: error.message });
+  }
+};
+export const editImageMarket: RequestHandler = async (req, res) => {
+  if (debug) console.log("#edit");
+  try {
+    const { secure_url: su, public_id: pi, _id } = req.body;
+    const image = await uploadImage({ secure_url: su, public_id: pi });
+    await MarketSch.findOneAndUpdate(
+      { "gallery._id": _id },
+      {
+        "gallery.$": image,
+      }
+    );
+    const result = await MarketSch.find();
+    res.send(result);
+  } catch (error: any) {
+    res.status(400).json({ msg: error.message });
+  }
+};
+export const deleteImageMarket: RequestHandler = async (req, res) => {
+  if (debug) console.log("#uploadImageMarket");
+  try {
+    const { _id, gal_id, public_id } = req.body;
+    await MarketSch.findOneAndUpdate(
+      { _id },
+      {
+        $pull: { gallery: { _id: gal_id } },
+      }
+    );
+    await deleteImage(public_id);
+
+    // const result = await MarketSch.findOne({ _id });
+    // if (result) {
+    //   result.gallery.forEach(async (gal, index) => {
+    //     if (gal._id?.toString() === gal_id) {
+    //       // await deleteImage(gal.public_id)
+    //       result.gallery.splice(index, 1);
+    //     }
+    //   });
+    //   // await result.save();
+    // }
+    // console.log(result);
+    const result2 = await MarketSch.find();
+    res.send(result2);
+  } catch (error: any) {
+    res.status(400).json({ msg: error.message });
+  }
+};
+
 export const createChart: RequestHandler = async (req, res) => {
   if (debug) console.log("#createChart");
   try {
@@ -136,7 +298,10 @@ export const createChart: RequestHandler = async (req, res) => {
 export const plusChart: RequestHandler = async (req, res) => {
   if (debug) console.log("#plusChart");
   try {
-    await ChartSch.findOneAndUpdate({ _id: "6682def3e5b948a57d5e16cc" }, { $inc: { amount: 1 } });
+    await ChartSch.findOneAndUpdate(
+      { _id: "6682def3e5b948a57d5e16cc" },
+      { $inc: { amount: 1 } }
+    );
     res.send(true);
   } catch (error: any) {
     res.status(400).json({ msg: error.message });
@@ -149,6 +314,44 @@ export const getChart: RequestHandler = async (req, res) => {
     res.send(result);
   } catch (error: any) {
     res.status(400).json({ msg: error.message });
+  }
+};
+
+export const register: RequestHandler = async (req, res) => {
+  if (debug) console.log("#register");
+  try {
+    const { name, email, pass } = req.body;
+
+    let findEmail = await UserSch.findOne({ email });
+    if (findEmail)
+      return res.status(409).json({ msg: "El correo ya esta en uso" });
+
+    let password = bcrypt.hashSync(pass, bcrypt.genSaltSync(10));
+    const result = await UserSch.create({ name, email, password });
+
+    const { name: n, email: e, _id } = result;
+    return res.send({ name: n, email: e, _id });
+  } catch (error: any) {
+    res.status(400).json({ msg: error.message });
+  }
+};
+
+export const login: RequestHandler = async (req, res) => {
+  if (debug) console.log("#login");
+  try {
+    const { email, pass } = req.body;
+
+    const user = await UserSch.findOne({ email });
+    if (!user) return res.status(401).json({ msg: "Correo incorrecto" });
+    bcrypt.compare(pass, user.password, function (_: any, result: any) {
+      if (!result)
+        return res.status(401).json({ msg: "Contraseña incorrecta" });
+      const { name, email, _id } = user;
+      return res.send({ name, email, _id });
+    });
+  } catch (error: any) {
+    console.log(error);
+    res.status(404).json({ msg: error.message });
   }
 };
 
--- src/models/MarketSch.ts
+++ src/models/MarketSch.ts
@@ -5,6 +5,7 @@ const MarketSch = new Schema(
   {
     name: { type: String, required: true },
     description: { type: String, required: true },
+    category: { type: String, required: true },
     logo: {
       secure_url: { type: String, required: true },
       public_id: { type: String, required: true },
